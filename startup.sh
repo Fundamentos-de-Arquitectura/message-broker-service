25c0bf239d9d0d481dc23c02bf7e61bbb3a762ae
Werner Lang <u202310003@upc.edu.pe>
2025-10-29 16:33:24 -0500

    fix: change sh for windows

--- startup.sh
+++ startup.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
-
-echo "Starting Kafka Infrastructure..."
+cd kafka-infrastructure
 docker-compose up -d
 
-echo "Waiting for Kafka to be ready..."
-sleep 15
+Start-Sleep -Seconds 15
 
-echo "Kafka is running!"
-echo "Kafka UI: http://localhost:8080"
-echo "Bootstrap servers: localhost:9092"
-
-echo "Creating topics..."
-docker exec kafka kafka-topics.sh --create \
-  --topic orders-events \
-  --bootstrap-server localhost:9092 \
-  --partitions 3 \
-  --replication-factor 1 \
+docker exec kafka kafka-topics.sh --create `
+  --topic orders-events `
+  --bootstrap-server localhost:9092 `
+  --partitions 3 `
+  --replication-factor 1 `
   --if-not-exists
 
-docker exec kafka kafka-topics.sh --create \
-  --topic inventory-events \
-  --bootstrap-server localhost:9092 \
-  --partitions 3 \
-  --replication-factor 1 \
+docker exec kafka kafka-topics.sh --create `
+  --topic inventory-events `
+  --bootstrap-server localhost:9092 `
+  --partitions 3 `
+  --replication-factor 1 `
   --if-not-exists
 
-echo "Topics created!"
+Write-Host "Kafka is running!"
+Write-Host "Kafka UI: http://localhost:8080"
+Write-Host "Bootstrap servers: localhost:9092"
